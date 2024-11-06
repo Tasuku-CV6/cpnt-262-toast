4f4b8c952e30f7fad7b2fb24608f1e365a422043
Tasuku-CV6 <Tasukuseikatsu45@gmail.com>
2024-11-06 09:10:06 -0700

    added steps on how to make toast

---
+++ toast.js
@@ -0,0 +1,21 @@
+// bread variable
+
+//isTimerDone
+
+// isBreadinToaster
+
+// isToast
+
+// toaster variable
+
+/* FUNCTIONS */
+
+// cageDown
+
+// cageUp
+
+// timer
+
+// activateHeat
+
+// removeToastFromToaster
